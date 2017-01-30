1dcad13b74435b35805c5986b2343554fe3b2c4e
Allen Wild <allenwild93@gmail.com>
2017-01-29 22:14:35 -0500

    add dd-to-sdcard.sh script

---
+++ dd-to-sdcard.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+[[ -n $1 ]] && disk=/dev/${1#/dev/}
+[[ -z $disk ]] && disk=/dev/sdi
+
+set -xe
+[[ -e ${disk}1 ]]
+[[ -e ${disk}2 ]]
+
+sudo dd if=core-image-newbs-raspberrypi3.boot.vfat of=${disk}1
+sudo dd if=core-image-newbs-raspberrypi3.squashfs-xz of=${disk}2
+sync
+sync
