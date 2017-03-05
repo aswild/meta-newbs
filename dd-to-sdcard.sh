35a50e0108951676fcd7d3b1cc3f26a149e95ab0
Allen Wild <allenwild93@gmail.com>
2017-03-05 14:50:47 -0500

    dd-to-sdcard: use bs=1M

--- dd-to-sdcard.sh
+++ dd-to-sdcard.sh
@@ -7,7 +7,7 @@ set -xe
 [[ -e ${disk}1 ]]
 [[ -e ${disk}2 ]]
 
-sudo dd if=core-image-newbs-raspberrypi3.boot.vfat of=${disk}1
-sudo dd if=core-image-newbs-raspberrypi3.squashfs-xz of=${disk}2
+sudo dd if=core-image-newbs-raspberrypi3.boot.vfat of=${disk}1 bs=1M
+sudo dd if=core-image-newbs-raspberrypi3.squashfs-xz of=${disk}2 bs=1M
 sync
 sync
