aa0e590ef09daa58234ad6a0a6e7a11110e8c889
Allen Wild <allenwild93@gmail.com>
2017-05-13 18:56:46 -0400

    dd-to-sdcard: support burning newbs-mce-image

--- dd-to-sdcard.sh
+++ dd-to-sdcard.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
-[[ -n $1 ]] && disk=/dev/${1#/dev/}
-[[ -z $disk ]] && disk=/dev/sdi
+disk=/dev/sdd
+image=core-image-newbs
+
+while [[ $# != 0 ]]; do
+    case $1 in
+        sd*)
+            disk=/dev/$1
+            ;;
+        /*)
+            disk=$1
+            ;;
+        mce)
+            image=newbs-mce-image
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
 
 set -xe
 [[ -e ${disk}1 ]]
 [[ -e ${disk}2 ]]
 
-sudo dd if=core-image-newbs-raspberrypi3.boot.vfat of=${disk}1 bs=1M
-sudo dd if=core-image-newbs-raspberrypi3.squashfs-xz of=${disk}2 bs=1M
+sudo dd if=${image}-raspberrypi3.boot.vfat of=${disk}1 bs=1M
+sudo dd if=${image}-raspberrypi3.squashfs-xz of=${disk}2 bs=1M
 sync
 sync
