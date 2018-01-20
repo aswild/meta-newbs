1d9183a0704a4c7256131f2c7bd3ca10270b9454
Allen Wild <allenwild93@gmail.com>
2018-01-20 16:11:29 -0500

    dd-to-sdcard: add some comments

--- dd-to-sdcard.sh
+++ dd-to-sdcard.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
+#
+# !WARNING!
+# Make sure you specify the correct disk! Overwriting the wrong drive would really suck.
+# !WARNING!
+#
+# SD CARD FORMATTING:
+# The bitbake output is separate filesystem images for /boot (vfat)
+# and the rootfs (squashfs or ext4). This script uses dd to install them
+# on a partitioned SD card.
+#
+# Use fdisk to partition the sdcard:
+#   p1 = boot partition. Type "W95 FAT32" (b). Size 40-100MB
+#   p2 = rootfs partition. Type "Linux" (83). Size: at least 250MB, but
+#        up to a gig allows for installing ext4 versions of larger images
+#   Neither of these partitions need to be formatted
+#
+# Optional but recommended: add a persistent /home partition. Format
+# it as ext4 with the label RPI-HOME, and create a /root directory
 
 disk=/dev/sdk
 image=core-image-newbs
 machine=raspberrypi3
+rootfstype=squashfs-xz
 
-while [[ $# != 0 ]]; do
+while (($#)); do
     case $1 in
         sd*)
             disk=/dev/$1
@@ -31,8 +50,6 @@ while [[ $# != 0 ]]; do
     esac
     shift
 done
-
-[[ -n $rootfstype ]] || rootfstype=squashfs-xz
 
 set -xe
 [[ -e ${disk}1 ]]
